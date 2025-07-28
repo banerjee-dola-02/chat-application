284bfed2ee60534cdff950662fea7a0d91f53f3d
banerjee-dola-02 <dolabanerjeedhn@gmail.com>
2025-07-28 23:53:00 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const newSocket = io("http://localhost:8080", { // Your backend URL
+      const newSocket = io("https://chat-application-backend-4lsc.onrender.com/", { // Your backend URL
         query: {
           userId: authUser._id,
         },
