94eccd42d406864f6f4cf523d42750dff7f66d0d
banerjee-dola-02 <dolabanerjeedhn@gmail.com>
2025-07-29 01:24:22 +0530

    Update OtherUsers.jsx

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import OtherUser from './OtherUser'
-import useGetOtherUsers from '../hooks/useGetOtherUsers';
-import { useSelector } from 'react-redux';
+// import React from 'react'
+// import OtherUser from './OtherUser'
+// import useGetOtherUsers from '../hooks/useGetOtherUsers';
+// import { useSelector } from 'react-redux';
 
 
-const OtherUsers = () => {
-  //my custom hook
-  useGetOtherUsers();
-  const {otherUsers}=useSelector(store=>store.user);
-  if(!otherUsers) return;    //early return in react
-  return (
-    <div className='overflow-auto flex-1'>
-        {
-          otherUsers?.map((user)=>{
-            return (
-              <OtherUser key={user._id} user={user}/>
-            )
-          })
-        }
-    </div>
-  )
+// const OtherUsers = () => {
+//   //my custom hook
+//   useGetOtherUsers();
+//   const {otherUsers}=useSelector(store=>store.user);
+//   if(!otherUsers) return;    //early return in react
+//   return (
+//     <div className='overflow-auto flex-1'>
+//         {
+//           otherUsers?.map((user)=>{
+//             return (
+//               <OtherUser key={user._id} user={user}/>
+//             )
+//           })
+//         }
+//     </div>
+//   )
+// }
+
+// export default OtherUsers
+
+
+
+import React from 'react';
+import OtherUser from './OtherUser';
+// We no longer need useSelector or the custom hook here
+
+const OtherUsers = ({ users }) => { // ✅ 1. Accept 'users' as a prop
+    
+    // Early return if the users array is not available
+    if (!users) return null;
+
+    return (
+        <div className='overflow-auto flex-1'>
+            {
+                // ✅ 2. Map over the 'users' array from props
+                users.map((user) => {
+                    return (
+                        <OtherUser key={user._id} user={user} />
+                    )
+                })
+            }
+        </div>
+    )
 }
 
-export default OtherUsers
+export default OtherUsers;
